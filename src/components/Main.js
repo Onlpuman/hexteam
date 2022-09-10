e6b9e36b9554ce8f5866e7c5832ad35e6ea11afb
Onlyhuman <onlyhuman@mail.ru>
2022-09-11 01:26:11 +0300

    Changed the implementation of requests to the server for correct error handling. Added error block display

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,11 +9,12 @@ import { sortByDigit } from '../lib/sortByDigit';
 import { copyToClipboard } from '../lib/copyToClipboard';
 
 import { Paginator } from './Paginator';
-import { ModalButtons } from './buttons/ModalButtons';
+import { ModalButtons } from './ModalButtons';
 
 
 export const Main = props => {
 	const { setAuthorized } = props;
+	const [error, setError] = useState(undefined);
 	const [statistics, setStatistics] = useState([]);
 	const [currentStatistics, setCurrentStatistics] = useState([]);
 	const [modalOpen, setModalOpen] = useState(false);
@@ -24,20 +25,20 @@ export const Main = props => {
 	const [sort, setSort] = useState('');
 	const limit = 10;
 
+
 	useEffect(() => {
 		axios
 			.get('http://79.143.31.216/statistics')
 			.then(response => setStatistics(response.data))
-			.catch(error => console.log(error));
+			.catch(error => setError(error?.response?.data?.detail || 'Unknown error'));
 	}, [squeezed]);
 
 	useEffect(() => {
 		axios
 			.get(`http://79.143.31.216/statistics?offset=${offset}&limit=${limit}`)
 			.then(response => setCurrentStatistics(response.data))
-			.catch(error => console.log(error));
+			.catch(error => setError(error?.response?.data?.detail || 'Unknown error'));
 	}, [offset, squeezed]);
-
 
 	const handleLogout = () => {
 		setAuthorized(false);
@@ -101,81 +102,89 @@ export const Main = props => {
 	};
 
 	return (
-		<>
-			<Paginator
-				statistics={statistics}
-				offset={offset}
-				setOffset={setOffset}
-			/>
-			<div className="buttons-container">
-				<Button onClick={handleModalOpen}>Squeeze link</Button>
-				<Button onClick={handleLogout}>Exit</Button>
-			</div>
-			<Table striped bordered hover>
-				<thead>
-					<tr>
-						<th>№</th>
-						<th onClick={handleSortByShortName}>Short link</th>
-						<th onClick={handleSortByName}>Original link</th>
-						<th onClick={handleSortByDigit}>Count of click</th>
-					</tr>
-				</thead>
-				<tbody>
-					{
-						currentStatistics.map((el, i) => {
-							return (
-								<tr key={el.id}>
-									<td>{offset + i + 1}</td>
-									<td
-										onClick={handleCopyLink}
-										data-link={`http://79.143.31.216/s/${el.short}`}
-									>
-										{`http://79.143.31.216/s/${el.short}`}
-									</td>
-									<td>{el.target}</td>
-									<td>{el.counter}</td>
-								</tr>
-							);
-						})
-					}
-				</tbody>
-			</Table>
-			{(
-				<Modal show={modalOpen} onHide={handleModalClose}>
-					<Modal.Header closeButton>
-						<Modal.Title>Create link</Modal.Title>
-					</Modal.Header>
-					<Modal.Body>
-						<Form>
-							<Form.Group className="mb-3">
-								<Form.Control
-									type="text"
-									placeholder={'Enter link to squeeze'}
-									value={squeezed ? `http://79.143.31.216/s/${squeezed}` : currentInput}
-									readOnly={recurringLink || squeezed}
-									onChange={(e) => setCurrentInput(e.target.value)}
-								/>
-							</Form.Group>
-							<ModalButtons
-								statistics={statistics}
-								currentInput={currentInput}
-								recurringLink={recurringLink}
-								squeezed={squeezed}
-								setSqueezed={setSqueezed}
-								setRecurringLink={setRecurringLink}
-								setCurrentInput={setCurrentInput}
-							/>
-							{recurringLink &&
-								<Alert variant={'danger'}>
-									Address already exists! Click on the "Try Again!" button.
-								</Alert>
+		error
+			? (
+				<Alert variant={'danger'}>
+					{error}
+				</Alert>
+			)
+			: (
+				<>
+					<Paginator
+						statistics={statistics}
+						offset={offset}
+						setOffset={setOffset}
+					/>
+					<div className="buttons-container">
+						<Button onClick={handleModalOpen}>Squeeze link</Button>
+						<Button onClick={handleLogout}>Exit</Button>
+					</div>
+					<Table striped bordered hover>
+						<thead>
+							<tr>
+								<th>№</th>
+								<th onClick={handleSortByShortName}>Short link</th>
+								<th onClick={handleSortByName}>Original link</th>
+								<th onClick={handleSortByDigit}>Count of click</th>
+							</tr>
+						</thead>
+						<tbody>
+							{
+								currentStatistics.map((el, i) => {
+									return (
+										<tr key={el.id}>
+											<td>{offset + i + 1}</td>
+											<td
+												onClick={handleCopyLink}
+												data-link={`http://79.143.31.216/s/${el.short}`}
+											>
+												{`http://79.143.31.216/s/${el.short}`}
+											</td>
+											<td>{el.target}</td>
+											<td>{el.counter}</td>
+										</tr>
+									);
+								})
 							}
-						</Form>
-					</Modal.Body>
-				</Modal>
-			)}
-			<span>*Click on the column name to sort</span>
-			<span>*Click on the short link to copy it</span>
-		</>
+						</tbody>
+					</Table>
+					{(
+						<Modal show={modalOpen} onHide={handleModalClose}>
+							<Modal.Header closeButton>
+								<Modal.Title>Create link</Modal.Title>
+							</Modal.Header>
+							<Modal.Body>
+								<Form>
+									<Form.Group className="mb-3">
+										<Form.Control
+											type="text"
+											placeholder={'Enter link to squeeze'}
+											value={squeezed ? `http://79.143.31.216/s/${squeezed}` : currentInput}
+											readOnly={recurringLink || squeezed}
+											onChange={(e) => setCurrentInput(e.target.value)}
+										/>
+									</Form.Group>
+									<ModalButtons
+										statistics={statistics}
+										currentInput={currentInput}
+										recurringLink={recurringLink}
+										squeezed={squeezed}
+										setSqueezed={setSqueezed}
+										setRecurringLink={setRecurringLink}
+										setCurrentInput={setCurrentInput}
+									/>
+									{recurringLink &&
+										<Alert variant={'danger'}>
+											Address already exists! Click on the "Try Again!" button.
+										</Alert>
+									}
+								</Form>
+							</Modal.Body>
+						</Modal>
+					)}
+					<span>*Click on the column name to sort</span>
+					<span>*Click on the short link to copy it</span>
+				</>
+			)
 	);
 };
